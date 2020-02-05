cb1d5a853930f49470e127e5e629fb3510072be4
Vlad Lutarevich <lutarevich95@gmail.com>
2020-02-05 21:03:11 +0300

    Updates

---
+++ precache-manifest.4f6ff3fcdad30ac175bc440802acd676.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bab84520b2d1c96d8c1192dd22d2078",
+    "url": "/demo-social-network/index.html"
+  },
+  {
+    "revision": "b42f1409697c3cdc958e",
+    "url": "/demo-social-network/static/css/3.e3469f91.chunk.css"
+  },
+  {
+    "revision": "2ab363b873ca29f7937a",
+    "url": "/demo-social-network/static/css/main.303c75df.chunk.css"
+  },
+  {
+    "revision": "b2427a5e0a170f6c4ede",
+    "url": "/demo-social-network/static/js/2.22275c69.chunk.js"
+  },
+  {
+    "revision": "b42f1409697c3cdc958e",
+    "url": "/demo-social-network/static/js/3.ffad667c.chunk.js"
+  },
+  {
+    "revision": "55e0818befcf29b69afd",
+    "url": "/demo-social-network/static/js/4.1a76f31b.chunk.js"
+  },
+  {
+    "revision": "2ab363b873ca29f7937a",
+    "url": "/demo-social-network/static/js/main.1472a02a.chunk.js"
+  },
+  {
+    "revision": "c80795fe0c069e742ba7",
+    "url": "/demo-social-network/static/js/runtime-main.bc7efecf.js"
+  },
+  {
+    "revision": "fceeb0fa26d3a4ea746b491d23625244",
+    "url": "/demo-social-network/static/media/preloader.fceeb0fa.svg"
+  },
+  {
+    "revision": "e080c08c2792a8cac333d17db3af16b7",
+    "url": "/demo-social-network/static/media/userProfile.e080c08c.png"
+  }
+]);
---
+++ static/js/3.ffad667c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-1-react-app"]=this["webpackJsonpmy-1-react-app"]||[]).push([[3],{294:function(t,e,n){t.exports={text:"Friends_text__1MdE-"}},295:function(t,e,n){"use strict";n.r(e);var a=n(0),s=n.n(a),c=n(294),p=n.n(c);e.default=function(){return s.a.createElement("div",{className:p.a.text},"Soon")}}}]);
+//# sourceMappingURL=3.ffad667c.chunk.js.map
